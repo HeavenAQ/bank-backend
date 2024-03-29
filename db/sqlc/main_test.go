5cc2773c032ad8d2bbed2499b1b4fa5490e36a83
Heaven Chen <891118heaven@gmail.com>
2024-03-29 09:58:04 +0800

    [Update] Prepare for adding tests

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:password@localhost:5432/simple-bank-db?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	dbpool, err := pgxpool.New(ctx, dbSource)
+	if err != nil {
+		log.Printf("Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+	m.Run()
+}
