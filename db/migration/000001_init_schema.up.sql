f3706b6914bc133e69e749bb787e3522ab535cc3
Heaven Chen <891118heaven@gmail.com>
2024-03-29 09:57:34 +0800

    [Setup] Set up database and simple CRUD queries

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "accounts" (
+    "id" bigserial PRIMARY KEY,
+    "owner" varchar NOT NULL,
+    "balance" bigint NOT NULL,
+    "currency" varchar NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now()),
+    "updated_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+    "id" bigserial PRIMARY KEY,
+    "account_id" bigint NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
+    "amount" bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now()),
+    "updated_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+    "id" bigserial PRIMARY KEY,
+    "from_account_id" bigint NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
+    "to_account_id" bigint NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
+    "amount" bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now()),
+    "updated_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
